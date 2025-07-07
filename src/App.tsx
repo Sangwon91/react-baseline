0abee3c1e8da6fa4935bcabbcb57f55fa7da6c69
Sangwon91 <lsw91.main@gmail.com>
2025-07-07 20:12:05 +0900

    Refactor App component to remove Router wrapper, simplifying the routing structure while maintaining layout integration for HomePage, BlogPage, BlogPostPage, and AboutPage.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import HomePage from './pages/HomePage'
 import BlogPage from './pages/BlogPage'
@@ -8,16 +8,14 @@ import './App.css'
 
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/blog" element={<BlogPage />} />
-          <Route path="/blog/:slug" element={<BlogPostPage />} />
-          <Route path="/about" element={<AboutPage />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/blog/:slug" element={<BlogPostPage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </Layout>
   )
 }
 
